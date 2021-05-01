b67420ab05996e40c588eda1edc24d3dfab50abb
rupam2001 <rjdasgalexy@gmail.com>
2021-05-01 12:27:26 +0530

    language settings pending, big fixes in buttons

--- src/components/editor2.js
+++ src/components/editor2.js
@@ -22,17 +22,30 @@ export default function Editor({ languages }) {
 
     const [prevText, setPrevText] = useState('')
 
+    const [keyPos, setKeyPos] = useState({ key: "", pos: 0 })
+
+    let _keyPos = { key: "", pos: 0 }
+
     useEffect(() => {
         textBoxRef.current.addEventListener("keydown", (event) => {
+
             if (event.ctrlKey && event.keyCode == 66) {
                 ApplyBold();
                 event.preventDefault();
                 event.stopPropagation();
             }
 
+            console.log(currentLanguage)
             if (currentLanguage.hasOwnProperty(event.key)) {
 
                 setCurrentSuggestion(currentLanguage[event.key])
+                event.preventDefault()
+                let curPos = getCaretIndex(textBoxRef.current)
+
+                const currentAlphaToShow = currentLanguage[event.key][0];
+
+                textBoxRef.current.innerHTML = textBoxRef.current.innerHTML.slice(0, curPos) + currentAlphaToShow + textBoxRef.current.innerHTML.slice(curPos, textBoxRef.current.innerHTML.length - 1)
+
             } else {
                 setCurrentSuggestion([])
             }
@@ -74,11 +87,7 @@ export default function Editor({ languages }) {
     }
 
     const ApplySizeInc = () => {
-
-        // setSelectedTextSize(selectedTextSize + 1)
         document.execCommand("size", false, "54px");
-
-
     }
     const ApplySizeDec = () => {
 
@@ -88,13 +97,14 @@ export default function Editor({ languages }) {
         let char = e.target.innerHTML
         setText(char)
 
+        const { x, y } = getCaretCoordinates()
+        console.log(x, y, "cooords")
     }
     const getSelectedTextSize = (selection) => {
         if (selection) {
 
             const size = window.getComputedStyle(selection.anchorNode.parentElement, null).getPropertyValue('font-size');
             console.log(size, "size")
-            // let num = size.match(//g)
             return size
         }
     }
@@ -128,7 +138,6 @@ export default function Editor({ languages }) {
             nodeMap[node.nodeName] = true
             node = node.parentNode
         }
-        console.log(nodeMap)
         return nodeMap
     }
 
@@ -144,15 +153,15 @@ export default function Editor({ languages }) {
         <div className="ed-wrapper">
             <div className="ed-tools">
                 <button onClick={ApplyBold} className="ed-tool-btn">
-                    <i className={currentParentsMap.hasOwnProperty("bold") ? "fa fa-bold active" : "fa fa-bold"} aria-hidden="true" id="bold"></i>
+                    <i className={currentParentsMap.hasOwnProperty("B") ? "fa fa-bold active" : "fa fa-bold"} aria-hidden="true" id="bold"></i>
                 </button>
 
                 <button onClick={ApplyItalic} className="ed-tool-btn">
-                    <i className={currentParentsMap.hasOwnProperty("italic") ? "fa fa-italic active" : "fa fa-italic"} aria-hidden="true" id="italic"></i>
+                    <i className={currentParentsMap.hasOwnProperty("I") ? "fa fa-italic active" : "fa fa-italic"} aria-hidden="true" id="italic"></i>
                 </button>
 
                 <button onClick={ApplyUnderLine} className="ed-tool-btn">
-                    <i className={currentParentsMap.hasOwnProperty("underline") ? "fa fa-underline active" : "fa fa-underline"} aria-hidden="true" id="underline"></i>
+                    <i className={currentParentsMap.hasOwnProperty("U") ? "fa fa-underline active" : "fa fa-underline"} aria-hidden="true" id="underline"></i>
                 </button>
 
                 <div class="dropdown">
@@ -171,7 +180,6 @@ export default function Editor({ languages }) {
                             }
                         </div>
                         <div className="ed-tool-color-wrapper">
-
                             <span>custom</span>
                             <button onClick={() => { }} >
                                 <input type="color" className="ed-tool-color-input"
@@ -180,7 +188,6 @@ export default function Editor({ languages }) {
                                     }} ></input>
                             </button>
                             <div className="ed-custome-colors">
-
                                 {
                                     customColors.map(c => (
                                         <button onClick={() => ApplyColor(c)} >
@@ -199,7 +206,6 @@ export default function Editor({ languages }) {
                     </button>
                     <div class="dropdown-content">
                         <div className="ed-custome-colors">
-
                             {
                                 colors.map(c => (
                                     <button className="ed-tools-btn" onClick={() => ApplyHighlighter(c)}>
@@ -209,7 +215,6 @@ export default function Editor({ languages }) {
                             }
                         </div>
                         <div className="ed-tool-color-wrapper">
-
                             <span>custom</span>
                             <button onClick={() => { }} >
                                 <input type="color" className="ed-tool-color-input" onChange={(e) => { setCurrentCustomeColor(e.target.value) }}
@@ -218,10 +223,9 @@ export default function Editor({ languages }) {
                                     }} ></input>
                             </button>
                             <div className="ed-custome-colors">
-
                                 {
                                     customColors.map(c => (
-                                        <button onClick={() => ApplyColor(c)} >
+                                        <button onClick={() => ApplyHighlighter(c)} >
                                             <i className="fa fa-highlighter" style={{ color: c }} ></i>
                                         </button>
                                     ))
@@ -259,17 +263,23 @@ export default function Editor({ languages }) {
                     </button>
                 </div> */}
 
+            </div>
 
+            <div style={{ position: 'relative' }}>
+
+                <div className="ed-main" innerHTML={text} contentEditable={true} id="ed-main" ref={textBoxRef} onInput={handleTextChange} onMouseUp={getSelectedText} spellCheck="false">
+                </div>
+                <div className="ed-suggestion">
+                    {
+                        currentSuggestion.map(alpha => (
+                            <span>{alpha}</span>
+                        ))
+                    }
+
+                </div>
             </div>
-            <div className="ed-main" innerHTML={text} contentEditable={true} id="ed-main" ref={textBoxRef} onInput={handleTextChange} onMouseUp={getSelectedText} spellCheck="false">
-            </div>
-            <div>
-                {
-                    currentSuggestion.map(alpha => (
-                        <span>{alpha}</span>
-                    ))
-                }
-            </div>
+
+
         </div>
     )
 }
@@ -287,4 +297,50 @@ const getDifference = (str1, str2) => {
         if (str1[i] != str2[i]) return i
     }
     return -1
+}
+
+
+function getCaretCoordinates() {
+    let x = 0,
+        y = 0;
+    const isSupported = typeof window.getSelection !== "undefined";
+    if (isSupported) {
+        const selection = window.getSelection();
+        if (selection.rangeCount !== 0) {
+            const range = selection.getRangeAt(0).cloneRange();
+            range.collapse(true);
+            const rect = range.getClientRects()[0];
+            if (rect) {
+                x = rect.left;
+                y = rect.top;
+            }
+        }
+    }
+    return { x, y };
+}
+function getCaretIndex(element) {
+    let position = 0;
+    const isSupported = typeof window.getSelection !== "undefined";
+    if (isSupported) {
+        const selection = window.getSelection();
+        if (selection.rangeCount !== 0) {
+            const range = window.getSelection().getRangeAt(0);
+            const preCaretRange = range.cloneRange();
+            preCaretRange.selectNodeContents(element);
+            preCaretRange.setEnd(range.endContainer, range.endOffset);
+            position = preCaretRange.toString().length;
+        }
+    }
+    return position;
+}
+function setCaret(el, pos) {
+
+    var range = document.createRange()
+    var sel = window.getSelection()
+
+    range.setStart(el.childNodes[pos], 5)
+    range.collapse(true)
+
+    sel.removeAllRanges()
+    sel.addRange(range)
 }
